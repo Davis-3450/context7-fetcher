24ae0721ea5bb07ca9b122be453c279925b25131
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-08-24 01:18:15 -0500

    add enum placeholders

---
+++ src/package/context7/models.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class SourceType(Enum):
+    """
+    The type of source that the project is from. 'type' in the API response.
+    """
+
+    REPO = "repo"
+    WEBSITE = "website"
+    # ...
